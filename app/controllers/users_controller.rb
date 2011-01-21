730f85cf6d136a90fb322e928696d133d7b26d6f
cypher109 <arthur.hylton@gmail.com>
2011-01-21 15:09:51 -0500

    Added a subscription list partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 	@messages = @user.messages
+	@subscriptions = @user.subscriptions.all(:include => :publisher)
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @user }
